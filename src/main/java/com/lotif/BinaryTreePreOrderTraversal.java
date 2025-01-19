b9ed576ae76bf95ae60a783910e175a49b0e4de3
roshan5977 <roshannayak5977@gmail.com>
2025-01-19 16:19:55 +0530

    init

---
+++ src/main/java/com/lotif/BinaryTreePreOrderTraversal.java
@@ -0,0 +1,21 @@
+package com.lotif;
+
+/**
+ * Given the root of a binary tree, return the preorder traversal of its nodes' values.
+ *
+ * Example 1:
+ *
+ * Input: root = [1,null,2,3]
+ *
+ * Output: [1,2,3]
+ *
+ * In this preoder
+ */
+public class BinaryTreePreOrderTraversal {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
